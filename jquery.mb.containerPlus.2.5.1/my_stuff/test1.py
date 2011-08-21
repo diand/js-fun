cbb55c741462a8a91ee8760ce4b79d7b79066bb9
andre.roberge <andre.roberge@0d7580f7-3884-158f-fb7e-15bc4f337001>
2011-08-21 18:16:32 +0000

    Adding jquery plugin to hack it.

---
+++ jquery.mb.containerPlus.2.5.1/my_stuff/test1.py
@@ -0,0 +1,3 @@
+class A(object):
+    def report(self):
+        print("Reporting from A")
---
+++ jquery.mb.containerPlus.2.5.1/my_stuff/test2.py
@@ -0,0 +1,8 @@
+from test1 import A
+
+class B(A):
+    def __init__(self):
+        pass
+
+b = B()
+b.report()
